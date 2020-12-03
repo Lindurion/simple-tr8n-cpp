d86849cc818a8f06a7ab3d7d754956b504ccbe86
Eric Barndollar <eric@EricWBarndollar.com>
2020-12-03 13:49:16 -0500

    Small fix for StringView target to determine C++17 vs. C++14 requirement.
    Realized SIMPLE_TR8N_STRING_VIEW_TYPE was being used for 2 different purposes, so renamed the other one to SIMPLE_TR8N_BASIC_STRING_VIEW_TYPE.

--- cmake/SimpleTr8nConfig.cmake
+++ cmake/SimpleTr8nConfig.cmake
@@ -6,7 +6,7 @@ mark_as_advanced(SIMPLE_TR8N_ENABLE_EXCEPTIONS)
 
 # simple_tr8n::string_view type:
 set(SIMPLE_TR8N_STRING_VIEW_TYPE "std" CACHE STRING
-    "simple_tr8n::string_view type to use")
+    "SimpleTr8n: string_view implementation to use (std, lite, or custom)")
 set_property(CACHE SIMPLE_TR8N_STRING_VIEW_TYPE PROPERTY STRINGS "std" "lite" "custom")
 
 set(SIMPLE_TR8N_STRING_VIEW_CUSTOM_TYPE "" CACHE STRING
@@ -23,12 +23,12 @@ mark_as_advanced(SIMPLE_TR8N_STRING_VIEW_CUSTOM_TARGET)
 
 if(SIMPLE_TR8N_STRING_VIEW_TYPE STREQUAL "std")
   set(SIMPLE_TR8N_STRING_VIEW_INCLUDE "<string_view>")
-  set(SIMPLE_TR8N_STRING_VIEW_TYPE "std::basic_string_view")
+  set(SIMPLE_TR8N_BASIC_STRING_VIEW_TYPE "std::basic_string_view")
 elseif(SIMPLE_TR8N_STRING_VIEW_TYPE STREQUAL "lite")
   set(SIMPLE_TR8N_REQUIRES_STRING_VIEW_LITE TRUE)
 
   set(SIMPLE_TR8N_STRING_VIEW_INCLUDE "<nonstd/string_view.hpp>")
-  set(SIMPLE_TR8N_STRING_VIEW_TYPE "nonstd::basic_string_view")
+  set(SIMPLE_TR8N_BASIC_STRING_VIEW_TYPE "nonstd::basic_string_view")
   set(SIMPLE_TR8N_STRING_VIEW_TARGET "nonstd::string-view-lite")
 elseif(SIMPLE_TR8N_STRING_VIEW_TYPE STREQUAL "custom")
   if(NOT SIMPLE_TR8N_STRING_VIEW_CUSTOM_TYPE)
@@ -36,7 +36,7 @@ elseif(SIMPLE_TR8N_STRING_VIEW_TYPE STREQUAL "custom")
   endif()
 
   set(SIMPLE_TR8N_STRING_VIEW_INCLUDE SIMPLE_TR8N_STRING_VIEW_CUSTOM_INCLUDE)
-  set(SIMPLE_TR8N_STRING_VIEW_TYPE SIMPLE_TR8N_STRING_VIEW_CUSTOM_TYPE)
+  set(SIMPLE_TR8N_BASIC_STRING_VIEW_TYPE SIMPLE_TR8N_STRING_VIEW_CUSTOM_TYPE)
   set(SIMPLE_TR8N_STRING_VIEW_TARGET SIMPLE_TR8N_STRING_VIEW_CUSTOM_TARGET)
 else()
   message(FATAL_ERROR "SIMPLE_TR8N_STRING_VIEW_TYPE must be std, lite, or custom")
--- src/simple_tr8n/string_view.hpp
+++ src/simple_tr8n/string_view.hpp
@@ -8,17 +8,17 @@
 
 namespace simple_tr8n {
 
-#ifndef SIMPLE_TR8N_STRING_VIEW_TYPE
+#ifndef SIMPLE_TR8N_BASIC_STRING_VIEW_TYPE
 
-#error "SIMPLE_TR8N_STRING_VIEW_TYPE must be defined"
+#error "SIMPLE_TR8N_BASIC_STRING_VIEW_TYPE must be defined"
 
 #else
 
 /** Type alias for std::basic_string_view or an equivalent implementation. */
 template<typename CharT>
-using basic_string_view = SIMPLE_TR8N_STRING_VIEW_TYPE<CharT>;
+using basic_string_view = SIMPLE_TR8N_BASIC_STRING_VIEW_TYPE<CharT>;
 
-#endif  // SIMPLE_TR8N_STRING_VIEW_TYPE
+#endif  // SIMPLE_TR8N_BASIC_STRING_VIEW_TYPE
 
 }  // namespace simple_tr8n
 
