abf94f9fb9d7abe8f3eaa9631671ef1d292780ed
Eric Barndollar <eric@EricWBarndollar.com>
2022-09-01 14:41:03 -0400

    Use CPM.cmake FetchContent wrapper

--- cmake/SimpleTr8nDeps.cmake
+++ cmake/SimpleTr8nDeps.cmake
@@ -2,30 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-include(FetchContent)
-
-## Like FetchContent_MakeAvailable, but adds EXCLUDE_FROM_ALL when invoking
-## add_subdirectory(). Accepts SOURCE_SUBDIR argument (which must be repeated,
-## since we don't want to access private FetchContent() state).
-macro(simple_tr8n_make_dep_available dep)
-  cmake_parse_arguments(ARG "" "SOURCE_SUBDIR" "" ${ARGN})
-
-  string(TOLOWER "${dep}" lowerCaseDep)
-  FetchContent_GetProperties(${dep})
-
-  if(NOT ${lowerCaseDep}_POPULATED)
-    FetchContent_Populate(${dep})
-
-    set(srcDir "${${lowerCaseDep}_SOURCE_DIR}")
-    if(NOT "${ARG_SOURCE_SUBDIR}" STREQUAL "")
-      string(APPEND srcDir "/${ARG_SOURCE_SUBDIR}")
-    endif()
-
-    add_subdirectory("${srcDir}" "${${lowerCaseDep}_BINARY_DIR}"
-        EXCLUDE_FROM_ALL)
-  endif()
-endmacro()
-
 ## Configures include paths from all targets within the given dir (and
 ## subdirectories) as SYSTEM (external) headers, so that warnings should not be
 ## reported for them.
